887078e12ade9a1b2fa3a2a6861f6146c93f5a83
tarou mame <mametarou963@outlook.jp>
2019-08-06 00:20:30 +0900

    取得した距離をM5Stackのコンソール(画面下に言った場合はクリア)およびシリアルに表示するように変更

--- M5StackUltrasonic.ino
+++ M5StackUltrasonic.ino
@@ -1,22 +1,47 @@
 #include <M5Stack.h>
-#include <HCSR04.h>
+#include <HCSR04.h> // 2～400cmまで取れる
 
 // triggerPinをpin2につなぎます
 // echoPinをpin5につなぎます
 UltraSonicDistanceSensor distanceSensor(2, 5);
 
+int lcdDisplayCount = 0;
+
+bool lcdDisplayMax(){
+  if(lcdDisplayCount >= 30){ 
+    return true;
+  }else{
+    return false;
+  }
+}
+
+void lcdDisplayReset(){
+  M5.Lcd.fillScreen(0x0);
+  M5.Lcd.setCursor(0, 0);
+  lcdDisplayCount = 0;
+  return;
+}
+
+
+
 // the setup routine runs once when M5Stack starts up
 void setup(){
   // Initialize the M5Stack object
   M5.begin();
+  Serial.begin(9600);
   Wire.begin();
-  // LCD display
-  M5.Lcd.printf("hello world");
 }
 
 // the loop routine runs over and over again forever
 void loop() {
-  M5.Lcd.println(distanceSensor.measureDistanceCm());
-  Serial.println("Testing DHT12 lib:");
-  delay(500);
+  double distance = distanceSensor.measureDistanceCm();
+  if( distance >= 0.0){
+    if(lcdDisplayMax()==true){
+      lcdDisplayReset();
+    }
+    M5.Lcd.println(distance);
+    Serial.println(distance);
+    lcdDisplayCount++;
+  }
+  delay(1000);
 }
